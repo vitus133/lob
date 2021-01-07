4608a77fed6d4741f3ae9145e21d80f0b171f552
Vitaly Grinberg <vgrinber@redhat.com>
2021-01-07 11:23:13 +0200

    Fix the URL

--- test.sh
+++ test.sh
@@ -6,10 +6,15 @@ echo "Script to enable rt kernel"
 echo "###################################"
     
 KERNEL_VER="4.18.0-193.31.1.rt13.81.el8_2.ocptest.x86_64"
-TEMPDIR=.
+TEMPDIR=$(mktemp -d)
+
 
 # Fetch required packages
-WEBROOT_RT="https://media.githubusercontent.com/media/vitus133/blob/main/"
+WEBROOT_RT="https://media.githubusercontent.com/media/vitus133/lob/master"
 for package in 'core' 'modules' 'modules-extra'; do
-  curl -sk ${WEBROOT_RT}/kernel-rt-${package}-${KERNEL_VER}.rpm -o kernel-rt-${package}-${KERNEL_VER}.rpm
+  curl -s ${WEBROOT_RT}/kernel-rt-${package}-${KERNEL_VER}.rpm -o ${TEMPDIR}/kernel-rt-${package}-${KERNEL_VER}.rpm
 done
+
+ls -al $TEMPDIR/
+
+rm -rf $TEMPDIR
